ecebdfddba32fe9a1ea68c37c157b28a3b0d414d
William Paulsen <bpswim90@gmail.com>
2017-11-08 15:58:45 -0600

    Implement printJobs and findByValue features.

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -87,6 +88,27 @@ public class JobData {
     /**
      * Read in data from a CSV file and store it in a list
      */
+    public static ArrayList<HashMap<String, String>> findByValue(String searchTerm) {
+
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+
+            for (Map.Entry<String, String> field : row.entrySet() ) {
+                String aValue = field.getValue();
+
+                if (aValue.contains(searchTerm)) {
+                    jobs.add(row);
+                    break;
+                }
+            }
+        }
+
+        return jobs;
+    }
+
     private static void loadData() {
 
         // Only load data once
