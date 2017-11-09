9f125809333e782cf5c8830b553e0e3229c167c0
William Paulsen <bpswim90@gmail.com>
2017-11-09 11:07:30 -0600

    List results are sorted alphabetically.

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -7,10 +7,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -44,6 +41,7 @@ public class JobData {
             }
         }
 
+        Collections.sort(values);
         return values;
     }
 
